d211f2bfff46e35dc74daec137a825f2afcbfc77
lucasnereslcs <lucasnereslcs@gmail.com>
2020-05-26 21:44:43 -0300

    Department Services

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,9 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentServices;
 
 public class DepartmentListController implements Initializable {
 
+	private DepartmentServices service;
+	
 	@FXML
 	private TableView<Department> tableViewDepartment;
 
@@ -26,6 +32,11 @@ public class DepartmentListController implements Initializable {
 
 	@FXML
 	private Button btNew;
+	
+	//para inserir a lista na tela na coluna Department
+	
+	private ObservableList <Department> obsList;
+	
 
 	// método para o botão
 	@FXML
@@ -51,5 +62,29 @@ public class DepartmentListController implements Initializable {
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
+	
+	//injetando dependencia do DepartmentServices	
+	public void setDepartmentServices (DepartmentServices service) {
+		this.service = service;
+	}
 
+	/* Este Método:
+	 * 
+	 * acessa o serviço
+	 * carrega o departamento 
+	 * e joga o departamento na ObservableList
+	 * 
+	 */
+	
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Servico está nulo");
+		}
+		
+		List<Department> list = service.findAll(); //Para carregar a lista de departamentos
+		obsList = FXCollections.observableArrayList(list); //carregando a lista no Observable List;
+		tableViewDepartment.setItems(obsList);//Para carregar os itens na TableView
+		
+			
+	}
 }
