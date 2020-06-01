374a174cd7980d3e20bb1e926d14033afa2a7525
lucasnereslcs <lucasnereslcs@gmail.com>
2020-06-01 20:32:20 -0300

    Tela para dialologo criar novo departamento

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentServices;
@@ -40,8 +49,9 @@ public class DepartmentListController implements Initializable {
 
 	// método para o botão
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 
 	@Override
@@ -86,5 +96,35 @@ public class DepartmentListController implements Initializable {
 		tableViewDepartment.setItems(obsList);//Para carregar os itens na TableView
 		
 			
+	}
+	
+	//Quando criamos uma janela de dialogo devemos dizer o stage que criou a janela
+	private void createDialogForm(String absoluteName, Stage parentStage) { 
+		//O argumento recebe o Stage da Janela que abriu a janelinha
+		//O absoluteName recebe o nome da View que vai carregar
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName)); // para carregar uma tela
+			Pane pane = loader.load();
+			
+			//Para carregar uma nova janelinha de dialogo na frente do Stage, eu preciso declarar um novo Stage
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department Data: "); //Para definir um nome para o titulo da tela
+			dialogStage.setScene(new Scene(pane)); // o painel será a nova cena
+			dialogStage.setResizable(false); //função que define se a janela pode (true) ou não (false) ser redimensionada
+			dialogStage.initOwner(parentStage); //quem é o pai dessa janela?? = parentStage
+			
+			dialogStage.initModality(Modality.WINDOW_MODAL); 
+			/* essa janela fica travada, enquanto vc não fechar a janela, 
+			 * vc não consegue aceessar a janela anterior
+			
+			*/
+			 dialogStage.showAndWait(); //mostrar e esperar
+			 
+			
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error load View", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
---
+++ src/gui/util/Utils.java
@@ -0,0 +1,30 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+// o objetivo dessa classe é abrir uma janela a partir da janela principal
+
+public class Utils {
+
+	//para acessar o Stage aonde o event está
+	public static Stage currentStage(ActionEvent event) {  // o argumento vai receber um evento que o botão vai executar
+		
+		return (Stage) ((Node)event.getSource()).getScene().getWindow();
+				
+				/*
+				 * getSource -> genérico
+				 * 
+				 * (Node) -> fazendo casting para Node
+				 * 
+				 * getScene() -> pega a cena
+				 * 
+				 * getWindow() -> pega a janela
+				 * 
+				 * getWindow() é uma superclasse do Stage e precisa ser convertida
+				 * por isso a necessidade do downcast
+				 * 
+				 */
+	}
+}
